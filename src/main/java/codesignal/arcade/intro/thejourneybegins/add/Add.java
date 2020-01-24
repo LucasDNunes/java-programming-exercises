96e2fa8827bd73f26df56c6d8be2503ded60f704
Lucas <lucasdomingosnunes@gmail.com>
2020-01-23 23:43:11 -0300

    first commit

---
+++ src/main/java/codesignal/arcade/intro/thejourneybegins/add/Add.java
@@ -0,0 +1,8 @@
+package codesignal.arcade.intro.thejourneybegins.add;
+
+public class Add {
+
+    public static int calculate(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+}
